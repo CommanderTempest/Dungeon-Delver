ff879f0a8c5c96d89081266931b1d24c60d0ecd3
CommanderTempest <krukovtempest@gmail.com>
2024-05-30 20:13:52 -0500

    Game UI
    +Defeat UI
    +Pause UI
    +Pause Input
    +Victory UI
    +Dynamic Labels

--- Scripts/Resources/StatResource.cs
+++ Scripts/Resources/StatResource.cs
@@ -6,6 +6,7 @@ using Godot;
 public partial class StatResource : Resource
 {
     public event Action OnZero; // signal
+    public event Action OnUpdate;
 
     [Export] public Stat StatType {get; private set;}
 
@@ -17,6 +18,8 @@ public partial class StatResource : Resource
         {
             _statValue = Mathf.Clamp(value, 0, Mathf.Inf);
 
+            OnUpdate.Invoke();
+            
             if (_statValue == 0)
             {
                 OnZero?.Invoke();
--- Scripts/UI/UIController.cs
+++ Scripts/UI/UIController.cs
@@ -7,6 +7,8 @@ public partial class UIController : Control
 {
   private Dictionary<ContainerType, UIContainer> containers;
 
+  private bool canPause = false;
+
     public override void _Ready()
     {
         containers = GetChildren().Where(
@@ -18,15 +20,63 @@ public partial class UIController : Control
         containers[ContainerType.Start].Visible = true;
 
         containers[ContainerType.Start].ButtonNode.Pressed += HandleStartPressed;
+        
+        GameEvents.OnEndGame += HandleEndGame;
+        GameEvents.OnVictory += HandleVictory;
+        containers[ContainerType.Pause].ButtonNode.Pressed += HandlePausePressed;
     }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!canPause) {return;}
+        if (!Input.IsActionJustPressed(GameConstants.INPUT_PAUSE))
+        {
+          return;
+        }
+
+        containers[ContainerType.Stats].Visible = GetTree().Paused;
+        GetTree().Paused = !GetTree().Paused;
+
+        containers[ContainerType.Pause].Visible = true;
+          
+    }
+
+    private void HandleVictory()
+    {
+        canPause = false;
+        containers[ContainerType.Stats].Visible = false;
+        containers[ContainerType.Vicotry].Visible = true;
+
+        GetTree().Paused = true;
+    }
+
+
+    private void HandleEndGame()
+    {
+        canPause = false;
+        containers[ContainerType.Stats].Visible = false;
+        containers[ContainerType.Defeat].Visible = true;
+    }
+
 
     private void HandleStartPressed()
     {
+      canPause = true;
+
       GetTree().Paused = false;
 
       containers[ContainerType.Start].Visible = false;
+      containers[ContainerType.Stats].Visible = true;
 
       GameEvents.RaiseStartGame();
+    }
+
+    private void HandlePausePressed()
+    {
+        GetTree().Paused = false;
+
+        containers[ContainerType.Pause].Visible = false;
+        containers[ContainerType.Stats].Visible = true;
     }
 
 }
